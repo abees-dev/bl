8f6d45e4e64c859f580335e545fd3882fc414779
Abees FM <abeesdevjs@gmail.com>
2025-02-20 21:36:57 +0700

    update auto eat

--- src/main/java/org/bleachhack/module/mods/AutoEat.java
+++ src/main/java/org/bleachhack/module/mods/AutoEat.java
@@ -8,6 +8,10 @@
  */
 package org.bleachhack.module.mods;
 
+import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.item.FoodComponent;
+import net.minecraft.item.FoodComponents;
+import net.minecraft.util.Hand;
 import org.bleachhack.event.events.EventTick;
 import org.bleachhack.eventbus.BleachSubscribe;
 import org.bleachhack.module.Module;
@@ -16,87 +20,92 @@ import org.bleachhack.setting.module.SettingSlider;
 import org.bleachhack.setting.module.SettingToggle;
 import org.bleachhack.util.InventoryUtils;
 
-import net.minecraft.entity.effect.StatusEffectCategory;
-import net.minecraft.item.FoodComponent;
-import net.minecraft.item.FoodComponents;
-import net.minecraft.util.Hand;
-
 public class AutoEat extends Module {
 
-	private boolean eating;
+    private boolean eating;
+    private int currentSelectedSlot;
 
-	public AutoEat() {
-		super("AutoEat", KEY_UNBOUND, ModuleCategory.PLAYER, "Automatically eats food for you.",
-				new SettingToggle("Hunger", true).withDesc("Eats when you're bewlow a certain amount of hunger.").withChildren(
-						new SettingSlider("Hunger", 0, 19, 14, 0).withDesc("The maximum hunger to eat at.")),
-				new SettingToggle("Health", false).withDesc("Eats when you're bewlow a certain amount of health.").withChildren(
-						new SettingSlider("Health", 0, 19, 14, 0).withDesc("The maximum health to eat at.")),
-				new SettingToggle("Gapples", true).withDesc("Eats golden apples.").withChildren(
-						new SettingToggle("Prefer", false).withDesc("Prefers golden apples avobe regular food.")),
-				new SettingToggle("Chorus", false).withDesc("Eats chorus fruit."),
-				new SettingToggle("Poisonous", false).withDesc("Eats poisonous food."));
-	}
+    public AutoEat() {
+        super("AutoEat", KEY_UNBOUND, ModuleCategory.PLAYER, "Automatically eats food for you.",
+                new SettingToggle("Hunger", true).withDesc("Eats when you're bewlow a certain amount of hunger.").withChildren(
+                        new SettingSlider("Hunger", 0, 19, 14, 0).withDesc("The maximum hunger to eat at.")),
+                new SettingToggle("Health", false).withDesc("Eats when you're bewlow a certain amount of health.").withChildren(
+                        new SettingSlider("Health", 0, 19, 14, 0).withDesc("The maximum health to eat at.")),
+                new SettingToggle("Gapples", true).withDesc("Eats golden apples.").withChildren(
+                        new SettingToggle("Prefer", false).withDesc("Prefers golden apples avobe regular food.")),
+                new SettingToggle("Chorus", false).withDesc("Eats chorus fruit."),
+                new SettingToggle("Poisonous", false).withDesc("Eats poisonous food."));
+    }
 
-	@Override
-	public void onDisable(boolean inWorld) {
-		mc.options.useKey.setPressed(false);
+    @Override
+    public void onDisable(boolean inWorld) {
+        mc.options.useKey.setPressed(false);
+        super.onDisable(inWorld);
+    }
 
-		super.onDisable(inWorld);
-	}
+    @Override
+    public void onEnable(boolean inWorld) {
+        super.onEnable(inWorld);
+    }
 
-	@BleachSubscribe
-	public void onTick(EventTick event) {
-		if (eating && mc.options.useKey.isPressed() && !mc.player.isUsingItem()) {
-			eating = false;
-			mc.options.useKey.setPressed(false);
-		}
+    @BleachSubscribe
+    public void onTick(EventTick event) {
+        if (eating && mc.options.useKey.isPressed() && !mc.player.isUsingItem()) {
+            eating = false;
+            mc.options.useKey.setPressed(false);
+            InventoryUtils.selectSlot(currentSelectedSlot);
+        }
 
-		if (getSetting(0).asToggle().getState() && mc.player.getHungerManager().getFoodLevel() <= getSetting(0).asToggle().getChild(0).asSlider().getValueInt()) {
-			startEating();
-		} else if (getSetting(1).asToggle().getState() && (int) mc.player.getHealth() + (int) mc.player.getAbsorptionAmount() <= getSetting(1).asToggle().getChild(0).asSlider().getValueInt()) {
-			startEating();
-		}
-	}
 
-	private void startEating() {
-		boolean gapples = getSetting(2).asToggle().getState();
-		boolean preferGapples = getSetting(2).asToggle().getChild(0).asToggle().getState();
-		boolean chorus = getSetting(3).asToggle().getState();
-		boolean poison = getSetting(4).asToggle().getState();
+        if (getSetting(0).asToggle().getState() && mc.player.getHungerManager().getFoodLevel() <= getSetting(0).asToggle().getChild(0).asSlider().getValueInt()) {
+            startEating();
+        } else if (getSetting(1).asToggle().getState() && (int) mc.player.getHealth() + (int) mc.player.getAbsorptionAmount() <= getSetting(1).asToggle().getChild(0).asSlider().getValueInt()) {
 
-		int slot = -1;
-		int hunger = -1;
-		for (int s: InventoryUtils.getInventorySlots(true)) {
-			FoodComponent food = mc.player.getInventory().getStack(s).getItem().getFoodComponent();
+            startEating();
+        }
+    }
 
-			if (food == null)
-				continue;
+    private void startEating() {
 
-			int h = preferGapples && (food == FoodComponents.GOLDEN_APPLE || food == FoodComponents.ENCHANTED_GOLDEN_APPLE)
-					? Integer.MAX_VALUE : food.getHunger();
+        boolean gapples = getSetting(2).asToggle().getState();
+        boolean preferGapples = getSetting(2).asToggle().getChild(0).asToggle().getState();
+        boolean chorus = getSetting(3).asToggle().getState();
+        boolean poison = getSetting(4).asToggle().getState();
 
-			if (h <= hunger
-					|| (!gapples && (food == FoodComponents.GOLDEN_APPLE || food == FoodComponents.ENCHANTED_GOLDEN_APPLE))
-					|| (!chorus && food == FoodComponents.CHORUS_FRUIT)
-					|| (!poison && isPoisonous(food)))
-				continue;
+        int slot = -1;
+        int hunger = -1;
+        for (int s : InventoryUtils.getInventorySlots(true)) {
+            FoodComponent food = mc.player.getInventory().getStack(s).getItem().getFoodComponent();
 
-			slot = s;
-			hunger = h;
-		}
+            if (food == null)
+                continue;
 
-		if (hunger != -1) {
-			if (slot == mc.player.getInventory().selectedSlot || slot == 40) {
-				mc.options.useKey.setPressed(true);
-				mc.interactionManager.interactItem(mc.player, slot == 40 ? Hand.OFF_HAND : Hand.MAIN_HAND);
-				eating = true;
-			} else {
-				InventoryUtils.selectSlot(slot);
-			}
-		}
-	}
+            int h = preferGapples && (food == FoodComponents.GOLDEN_APPLE || food == FoodComponents.ENCHANTED_GOLDEN_APPLE)
+                    ? Integer.MAX_VALUE : food.getHunger();
 
-	private boolean isPoisonous(FoodComponent food) {
-		return food.getStatusEffects().stream().anyMatch(e -> e.getFirst().getEffectType().getCategory() == StatusEffectCategory.HARMFUL);
-	}
+            if (h <= hunger
+                    || (!gapples && (food == FoodComponents.GOLDEN_APPLE || food == FoodComponents.ENCHANTED_GOLDEN_APPLE))
+                    || (!chorus && food == FoodComponents.CHORUS_FRUIT)
+                    || (!poison && isPoisonous(food)))
+                continue;
+
+            slot = s;
+            hunger = h;
+        }
+
+        if (hunger != -1) {
+            if (slot == mc.player.getInventory().selectedSlot || slot == 40) {
+                mc.options.useKey.setPressed(true);
+                mc.interactionManager.interactItem(mc.player, slot == 40 ? Hand.OFF_HAND : Hand.MAIN_HAND);
+                eating = true;
+            } else {
+                currentSelectedSlot = mc.player.getInventory().selectedSlot;
+                InventoryUtils.selectSlot(slot);
+            }
+        }
+    }
+
+    private boolean isPoisonous(FoodComponent food) {
+        return food.getStatusEffects().stream().anyMatch(e -> e.getFirst().getEffectType().getCategory() == StatusEffectCategory.HARMFUL);
+    }
 }
